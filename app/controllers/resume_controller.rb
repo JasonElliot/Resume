d7b92cb6c935f802e4cc34c07360d7e021aa459c
Jason Elliot <elliotjjason@gmail.com>
2019-03-22 17:04:32 +0530

    initial commit

---
+++ app/controllers/resume_controller.rb
@@ -0,0 +1,24 @@
+class  ResumeController < ApplicationController
+
+
+def home
+
+end
+
+def profile
+
+end
+
+def projects
+
+end
+
+def contact
+
+end
+
+def resumae
+
+end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'resume#home'
+get 'profile', to: 'resume#profile'
+get 'projects', to: 'resume#projects'
+get 'resumae', to: 'resume#resumae'
+get 'contact', to: 'resume#contact'
+end
